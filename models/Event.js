0a8dcf2e8b1d29218d7b39a0906cfded93532541
archi-cell <159535338+archi-cell@users.noreply.github.com>
2026-02-28 14:38:41 +0530

    Create Event.js

---
+++ models/Event.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: String,
+            required: true
+        },
+        location: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        image: {
+            type: String, // store image URL
+            required: true
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model("Event", eventSchema);
